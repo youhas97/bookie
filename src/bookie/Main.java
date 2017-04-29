ceaaf7da295147004afe9c81eb0cccc2b5887080
johca907 <johca907@student.liu.se>
2017-04-29 20:17:47 +0200

    Added features: new user new calendar show current calendar almost book change current calendar

--- src/bookie/Main.java
+++ src/bookie/Main.java
@@ -5,18 +5,27 @@ import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-	User bertil = new User("Bertil");
-	Calendar cal = new Calendar(bertil, "TestCal");
+	User can = new User("Can");
+	User hashem = new User("Hashem");
+
+	Calendar canHome = new Calendar(can, "Home");
+	Calendar canWork = new Calendar(can, "Work");
+
+	Calendar hashemHome = new Calendar(hashem, "Jalla Home");
+	Calendar hashemWork = new Calendar(hashem, "Jalla Work");
+
 	TimeSpan span = new TimeSpan(LocalTime.of(16, 04), LocalTime.of(18, 31));
 	TimeSpan span2 = new TimeSpan(LocalTime.of(19, 00), LocalTime.of(21, 00));
 	TimeSpan span3 = new TimeSpan(LocalTime.of(11, 04), LocalTime.of(13, 00));
-	cal.book(LocalDate.of(2017, 07, 16), span, "Fika med Pär-Olof");
-	cal.book(LocalDate.of(2017, 07, 16), span2, "Fika med Pär-Olof");
-	cal.book(LocalDate.of(2017, 07, 16), span3, "Fika med Pär-Olof");
-	cal.show();
-	System.out.println(cal);
+
+	canWork.book(LocalDate.of(2017, 07, 16), span, "Jobba");
+	canHome.book(LocalDate.of(2017, 07, 16), span2, "Fika med Pär-Olof");
+	canHome.book(LocalDate.of(2017, 07, 16), span3, "Köpa bästa bulle");
+
+	hashemWork.book(LocalDate.of(2017, 07, 16), span, "Jobba");
+	hashemHome.book(LocalDate.of(2017, 07, 16), span2, "El has bezazi");
+	hashemHome.book(LocalDate.of(2017, 07, 16), span3, "Köpa bästa eire");
 
 	CalendarFrame calFrame = new CalendarFrame();
-	calFrame.showCalendar(cal);
     }
 }
--- src/bookie/User.java
+++ src/bookie/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class User {
     private String name;
+    private List<Calendar> calendars = new ArrayList<>();
     private static List<User> existingUsers = new ArrayList<>();
 
     public User(final String name) {
@@ -12,12 +13,26 @@ public class User {
 	existingUsers.add(this);
     }
 
-    @Override
-    public String toString() {
+    public List<Calendar> getCalendars() {
+	return calendars;
+    }
+
+    public String getName() {
 	return name;
     }
 
     public static List<User> getExistingUsers() {
 	return existingUsers;
+    }
+
+    @Override public String toString() {
+	return name;
+    }
+
+    public void addCalendar(Calendar cal) {
+	if (!Calendar.isExistingCalendar(cal)) {
+	    calendars.add(cal);
+	    System.out.println("Calendar created");
+	} else System.out.println("Calendar already exists");
     }
 }
